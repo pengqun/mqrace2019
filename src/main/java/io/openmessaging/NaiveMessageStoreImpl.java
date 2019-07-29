001723068754d76599c2f47b5c04c74ecb3fd880
楚衡 <pengqun.pq@alibaba-inc.com>
2019-07-29 23:52:42 +0800

    update log

--- src/main/java/io/openmessaging/NaiveMessageStoreImpl.java
+++ src/main/java/io/openmessaging/NaiveMessageStoreImpl.java
@@ -13,25 +13,38 @@ import java.util.concurrent.atomic.AtomicLong;
 public class NaiveMessageStoreImpl extends MessageStore {
 
     private static final Logger logger = Logger.getLogger(NaiveMessageStoreImpl.class);
+
     private AtomicLong counter = new AtomicLong();
+    private long _aMin = Long.MAX_VALUE;
+    private long _aMax = Long.MIN_VALUE;
+    private long _tMin = Long.MAX_VALUE;
+    private long _tMax = Long.MIN_VALUE;
 
     @Override
-    public void put(Message message) {
-        if (counter.incrementAndGet() < 100000) {
-            logger.info("t - " + message.getT() + ", a - " + message.getA() + ", body.length - " + message.getBody().length);
-        }
+    public synchronized void put(Message message) {
+//        if (counter.incrementAndGet() < 100000) {
+//            logger.info("t - " + message.getT() + ", a - " + message.getA() + ", body.length - " + message.getBody().length);
+//        }
+        counter.incrementAndGet();
+        _aMin = Math.min(message.getA(), _aMin);
+        _aMax = Math.max(message.getA(), _aMax);
+        _tMin = Math.min(message.getT(), _tMin);
+        _tMax = Math.max(message.getT(), _tMax);
     }
 
     @Override
     public List<Message> getMessage(long aMin, long aMax, long tMin, long tMax) {
-        logger.info("getMessage: aMin - " + aMin + ", aMax - " + aMax + ", tMin - " + tMin + ", tMax - " + tMax);
+//        logger.info("getMessage: aMin - " + aMin + ", aMax - " + aMax + ", tMin - " + tMin + ", tMax - " + tMax);
+        logger.info("getMessage: _aMin - " + _aMin + ", _aMax - " + _aMax
+                + ", _tMin - " + _tMin + ", _tMax - " + _tMax + ", counter = " + counter);
+
         ArrayList<Message> res = new ArrayList<Message>();
         return res;
     }
 
     @Override
     public long getAvgValue(long aMin, long aMax, long tMin, long tMax) {
-        logger.info("getAvgValue: aMin - " + aMin + ", aMax - " + aMax + ", tMin - " + tMin + ", tMax - " + tMax);
+//        logger.info("getAvgValue: aMin - " + aMin + ", aMax - " + aMax + ", tMin - " + tMin + ", tMax - " + tMax);
         long sum = 0;
         long count = 0;
         return count == 0 ? 0 : sum / count;

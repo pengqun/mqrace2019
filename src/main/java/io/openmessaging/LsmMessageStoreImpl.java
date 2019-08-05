74343d46acd5e87b62dc27e067eee3985c1cb1aa
楚衡 <pengqun.pq@alibaba-inc.com>
2019-08-05 22:08:29 +0800

    set memtable size to 0.1m

--- src/main/java/io/openmessaging/LsmMessageStoreImpl.java
+++ src/main/java/io/openmessaging/LsmMessageStoreImpl.java
@@ -19,7 +19,7 @@ public class LsmMessageStoreImpl extends MessageStore {
 
     private static final Logger logger = Logger.getLogger(LsmMessageStoreImpl.class);
 
-    private static final int MAX_MEM_TABLE_SIZE = 1000000;
+    private static final int MAX_MEM_TABLE_SIZE = 100000;
 
     static {
         logger.info("LsmMessageStoreImpl loaded");

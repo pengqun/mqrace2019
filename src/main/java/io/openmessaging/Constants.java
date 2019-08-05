71b614f5f62a7d18fe5b5f8dcec0235fa1438a74
楚衡 <pengqun.pq@alibaba-inc.com>
2019-08-05 19:53:58 +0800

    fix access level

--- src/main/java/io/openmessaging/Constants.java
+++ src/main/java/io/openmessaging/Constants.java
@@ -3,10 +3,10 @@ package io.openmessaging;
 /**
  * @author pengqun.pq 2019-07-29
  */
-public class Constants {
-    private static final String DATA_DIR = "/alidata1/race2019/data/";
-    public static final int MSG_BYTE_LENGTH = 50;
-    public static final int BODY_BYTE_LENGTH = 34;
+class Constants {
+    static final String DATA_DIR = "/alidata1/race2019/data/";
+    static final int MSG_BYTE_LENGTH = 50;
+    static final int BODY_BYTE_LENGTH = 34;
 
 //    public static final String DATA_DIR = "/tmp/";
 //    public static final int MSG_BYTE_LENGTH = 24;

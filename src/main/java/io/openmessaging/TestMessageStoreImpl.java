ec7887cc94ae0f7258b47e005b361c8770af02e3
楚衡 <pengqun.pq@alibaba-inc.com>
2019-08-07 17:06:47 +0800

    Add stats

--- src/main/java/io/openmessaging/TestMessageStoreImpl.java
+++ src/main/java/io/openmessaging/TestMessageStoreImpl.java
@@ -5,7 +5,6 @@ import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author .ignore 2019-07-29
@@ -14,29 +13,52 @@ public class TestMessageStoreImpl extends MessageStore {
 
     private static final Logger logger = Logger.getLogger(TestMessageStoreImpl.class);
 
-    private AtomicLong counter = new AtomicLong();
+    private long totalCount = 0;
     private long _aMin = Long.MAX_VALUE;
     private long _aMax = Long.MIN_VALUE;
     private long _tMin = Long.MAX_VALUE;
     private long _tMax = Long.MIN_VALUE;
+
+    private long minDiff = Long.MAX_VALUE;
+    private long maxDiff = Long.MIN_VALUE;
+    private long totalPosDiff = 0;
+    private long posDiffCount = 0;
+    private long totalNegDiff = 0;
+    private long negDiffCount = 0;
 
     @Override
     public synchronized void put(Message message) {
 //        if (counter.incrementAndGet() < 100000) {
 //            logger.info("t - " + message.getT() + ", a - " + message.getA() + ", body.length - " + message.getBody().length);
 //        }
-        counter.incrementAndGet();
+        totalCount++;
         _aMin = Math.min(message.getA(), _aMin);
         _aMax = Math.max(message.getA(), _aMax);
         _tMin = Math.min(message.getT(), _tMin);
         _tMax = Math.max(message.getT(), _tMax);
+
+        long diff = message.getT() - message.getA();
+        minDiff = Math.min(diff, minDiff);
+        maxDiff = Math.max(diff, maxDiff);
+        if (diff > 0) {
+            totalPosDiff += diff;
+            posDiffCount++;
+        } else if (diff < 0) {
+            totalNegDiff += diff;
+            negDiffCount++;
+        }
     }
 
     @Override
     public List<Message> getMessage(long aMin, long aMax, long tMin, long tMax) {
 //        logger.info("getMessage: aMin - " + aMin + ", aMax - " + aMax + ", tMin - " + tMin + ", tMax - " + tMax);
         logger.info("getMessage: _aMin - " + _aMin + ", _aMax - " + _aMax
-                + ", _tMin - " + _tMin + ", _tMax - " + _tMax + ", counter = " + counter);
+                + ", _tMin - " + _tMin + ", _tMax - " + _tMax + ", totalCount = " + totalCount
+                + ", minDiff - " + minDiff + ", maxDiff - " + maxDiff
+                + ", posDiffCount - " + posDiffCount + ", negDiffCount - " + negDiffCount
+                + ", avgPosDiff - " + (posDiffCount > 0 ? totalPosDiff / posDiffCount : 0)
+                + ", avgNegDiff - " + (negDiffCount > 0 ? totalNegDiff / negDiffCount : 0)
+        );
 
         ArrayList<Message> res = new ArrayList<Message>();
         return res;
@@ -44,7 +66,7 @@ public class TestMessageStoreImpl extends MessageStore {
 
     @Override
     public long getAvgValue(long aMin, long aMax, long tMin, long tMax) {
-//        logger.info("getAvgValue: aMin - " + aMin + ", aMax - " + aMax + ", tMin - " + tMin + ", tMax - " + tMax);
+        logger.info("getAvgValue: aMin - " + aMin + ", aMax - " + aMax + ", tMin - " + tMin + ", tMax - " + tMax);
         long sum = 0;
         long count = 0;
         return count == 0 ? 0 : sum / count;

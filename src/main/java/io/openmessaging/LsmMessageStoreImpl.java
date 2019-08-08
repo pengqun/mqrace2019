fe156fc5118318b0e278170d4d7d4e65678c610d
楚衡 <pengqun.pq@alibaba-inc.com>
2019-08-08 13:30:58 +0800

    Set write buffer size to 10w

--- src/main/java/io/openmessaging/LsmMessageStoreImpl.java
+++ src/main/java/io/openmessaging/LsmMessageStoreImpl.java
@@ -24,13 +24,13 @@ public class LsmMessageStoreImpl extends MessageStore {
 
     private static final Logger logger = Logger.getLogger(LsmMessageStoreImpl.class);
 
-    private static final int MAX_MEM_TABLE_SIZE = 1000000;
+    private static final int MAX_MEM_TABLE_SIZE = 100000;
 
     private static final int SST_FILE_INDEX_RATE = 32;
 
-    private static final int WRITE_BUFFER_SIZE = Constants.MSG_BYTE_LENGTH * 1000;
+    private static final int WRITE_BUFFER_SIZE = Constants.MSG_BYTE_LENGTH * 10000;
     private static final int READ_BUFFER_SIZE = Constants.MSG_BYTE_LENGTH * 1000;
-    private static final int WRITE_TA_BUFFER_SIZE = Constants.TA_BYTE_LENGTH * 1000;
+    private static final int WRITE_TA_BUFFER_SIZE = Constants.TA_BYTE_LENGTH * 10000;
     private static final int READ_TA_BUFFER_SIZE = Constants.TA_BYTE_LENGTH * 1000;
 
     private static final int PERSIST_SAMPLE_RATE = 100;

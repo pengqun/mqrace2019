12dc64311165e43ccee1af58e40b1325c094061d
楚衡 <pengqun.pq@alibaba-inc.com>
2019-08-08 11:25:48 +0800

    Set read ta buffer size to 1000

--- src/main/java/io/openmessaging/LsmMessageStoreImpl.java
+++ src/main/java/io/openmessaging/LsmMessageStoreImpl.java
@@ -31,7 +31,7 @@ public class LsmMessageStoreImpl extends MessageStore {
     private static final int WRITE_BUFFER_SIZE = Constants.MSG_BYTE_LENGTH * 1000;
     private static final int READ_BUFFER_SIZE = Constants.MSG_BYTE_LENGTH * 1000;
     private static final int WRITE_TA_BUFFER_SIZE = Constants.TA_BYTE_LENGTH * 1000;
-    private static final int READ_TA_BUFFER_SIZE = Constants.TA_BYTE_LENGTH * 5000;
+    private static final int READ_TA_BUFFER_SIZE = Constants.TA_BYTE_LENGTH * 1000;
 
     private static final int PERSIST_SAMPLE_RATE = 100;
     private static final int PUT_SAMPLE_RATE = 10000000;

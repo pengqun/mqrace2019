8d5b50fff9b1847c314d22c664c2482ee49a20ec
楚衡 <pengqun.pq@alibaba-inc.com>
2019-08-12 15:03:58 +0800

    Increase read buffer size

--- src/main/java/io/openmessaging/LsmMessageStoreImpl.java
+++ src/main/java/io/openmessaging/LsmMessageStoreImpl.java
@@ -41,7 +41,7 @@ public class LsmMessageStoreImpl extends MessageStore {
     private static final int READ_BUFFER_SIZE = Constants.MSG_BYTE_LENGTH * 1024;
 
     private static final int WRITE_A_BUFFER_SIZE = Constants.KEY_A_BYTE_LENGTH * 2048;
-    private static final int READ_A_BUFFER_SIZE = Constants.KEY_A_BYTE_LENGTH * 2048;
+    private static final int READ_A_BUFFER_SIZE = Constants.KEY_A_BYTE_LENGTH * 4096;
 
     private static final int PERSIST_SAMPLE_RATE = 100;
     private static final int PUT_SAMPLE_RATE = 10000000;
@@ -296,6 +296,7 @@ public class LsmMessageStoreImpl extends MessageStore {
         long sum = 0;
         long count = 0;
         long skip = 0;
+        long zero = 0;
 
         long offset = tSummary[(int) (tMin / T_INDEX_SUMMARY_RATE)];
         for (int t = (int) (tMin / T_INDEX_SUMMARY_RATE * T_INDEX_SUMMARY_RATE); t < tMin; t++) {
@@ -308,6 +309,9 @@ public class LsmMessageStoreImpl extends MessageStore {
 
         for (int t = (int) tMin; t <= tMax; t++) {
             int aCount = tIndex[t];
+            if (aCount == 0) {
+                zero++;
+            }
             while (aCount-- > 0) {
                 if (aByteBufferForRead.remaining() == 0) {
                     int bytes = 0;
@@ -332,7 +336,8 @@ public class LsmMessageStoreImpl extends MessageStore {
         aByteBufferForRead.clear();
 
         if (avgId % AVG_SAMPLE_RATE == 0) {
-            logger.info("Got " + count + " values, skip " + skip + ", time: " + (System.currentTimeMillis() - avgStart));
+            logger.info("Got " + count + ", skip: " + skip + ", zero: " + zero
+                    + ", time: " + (System.currentTimeMillis() - avgStart));
         }
         if (IS_TEST_RUN) {
             avgMsgCounter.addAndGet((int) count);
@@ -340,7 +345,7 @@ public class LsmMessageStoreImpl extends MessageStore {
         return count == 0 ? 0 : sum / count;
     }
 
-    private int findStartIndex(SSTableFile file, long tMin, long tMax) throws IOException {
+    private int findStartIndex(SSTableFile file, long tMin, long tMax) {
         if (tMin <= file.tStart) {
             return 0;
         }

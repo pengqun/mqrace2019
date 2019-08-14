2fcf42aa4870c097f1b2bb9033361c660d966eee
楚衡 <pengqun.pq@alibaba-inc.com>
2019-08-14 16:01:58 +0800

    test

--- src/main/java/io/openmessaging/MiMessageStoreImpl.java
+++ src/main/java/io/openmessaging/MiMessageStoreImpl.java
@@ -26,10 +26,10 @@ public class MiMessageStoreImpl extends MessageStore {
 
     private static final Logger logger = Logger.getLogger(MiMessageStoreImpl.class);
 
-    private static final int MAX_MEM_TABLE_SIZE = 10 * 10000;
+    private static final int MAX_MEM_TABLE_SIZE = 20 * 10000;
 
     private static final int T_INDEX_SIZE = 1000 * 1024 * 1024;
-    private static final int T_INDEX_SUMMARY_RATE = 8;
+    private static final int T_INDEX_SUMMARY_RATE = 32;
     private static final int T_WRITE_ARRAY_SIZE = 400 * 10000;
 
     private static final int A_DIFF_BASE_OFFSET = 10000;

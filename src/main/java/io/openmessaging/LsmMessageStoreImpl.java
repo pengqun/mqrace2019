6bb0b31f1c23dd86a7a8fcd3ed2a57519754bc64
楚衡 <pengqun.pq@alibaba-inc.com>
2019-08-19 23:56:01 +0800

    test

--- src/main/java/io/openmessaging/LsmMessageStoreImpl.java
+++ src/main/java/io/openmessaging/LsmMessageStoreImpl.java
@@ -24,7 +24,7 @@ public class LsmMessageStoreImpl extends MessageStore {
 
     private static final Logger logger = Logger.getLogger(LsmMessageStoreImpl.class);
 
-    private static final int MAX_MEM_TABLE_SIZE = 15 * 10000;
+    private static final int MAX_MEM_TABLE_SIZE = 10 * 10000;
     private static final int PERSIST_BUFFER_SIZE = 5 * 1024 * 1024;
 
     private static final int T_INDEX_SIZE = 1200 * 1024 * 1024;
@@ -33,7 +33,7 @@ public class LsmMessageStoreImpl extends MessageStore {
     private static final int WRITE_A_BUFFER_SIZE = Constants.KEY_A_BYTE_LENGTH * 1024;
     private static final int READ_A_BUFFER_SIZE = Constants.KEY_A_BYTE_LENGTH * 128;
 
-    private static final int WRITE_BODY_BUFFER_SIZE = Constants.BODY_BYTE_LENGTH * 1024;
+    private static final int WRITE_BODY_BUFFER_SIZE = Constants.BODY_BYTE_LENGTH * 128;
     private static final int READ_BODY_BUFFER_SIZE = Constants.BODY_BYTE_LENGTH * 128;
 
     private static final int PERSIST_SAMPLE_RATE = 100;

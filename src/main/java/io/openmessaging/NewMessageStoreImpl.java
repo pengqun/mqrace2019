857cf80dbf77299c2cdbac91a8ee2ec4344635e3
楚衡 <pengqun.pq@alibaba-inc.com>
2019-08-25 11:34:15 +0800

    Try wait/notify

--- src/main/java/io/openmessaging/NewMessageStoreImpl.java
+++ src/main/java/io/openmessaging/NewMessageStoreImpl.java
@@ -29,7 +29,7 @@ public class NewMessageStoreImpl extends MessageStore {
     private static final long A_UPPER_LIMIT = Long.MAX_VALUE;
     private static final int MSG_COUNT_UPPER_LIMIT = Integer.MAX_VALUE;
 
-    private static final int MAX_MEM_BUFFER_SIZE = 64 * 1024;
+    private static final int MAX_MEM_BUFFER_SIZE = 256 * 1024;
 
     private static final int PERSIST_BUFFER_SIZE = 2 * 1024 * 1024;
 
@@ -114,7 +114,7 @@ public class NewMessageStoreImpl extends MessageStore {
 //        }
 
         if (putId >= bufferOverflowLimit) {
-//            int waitTimes = 0;
+            int waitTimes = 0;
             synchronized (bufferAvailableLock) {
                 while (putId >= bufferOverflowLimit) {
                     try {
@@ -122,9 +122,9 @@ public class NewMessageStoreImpl extends MessageStore {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-//                    if (waitTimes++ > 5) {
-//                        throw new RuntimeException("timeout");
-//                    }
+                    if (waitTimes++ > 5) {
+                        throw new RuntimeException("timeout");
+                    }
                 }
             }
         }

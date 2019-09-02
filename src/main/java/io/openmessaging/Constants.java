359aa4fd813bba2dc97e460e512eb868b8a6efc9
楚衡 <pengqun.pq@alibaba-inc.com>
2019-09-03 06:35:41 +0800

    index 4k 1k 32

--- src/main/java/io/openmessaging/Constants.java
+++ src/main/java/io/openmessaging/Constants.java
@@ -16,7 +16,7 @@ class Constants {
     static final int T_INDEX_SUMMARY_FACTOR = 64;
 
     static final int A_INDEX_MAIN_BLOCK_SIZE = 1024 * 4;
-    static final int A_INDEX_SUB_BLOCK_SIZE = 1024 * 2;
+    static final int A_INDEX_SUB_BLOCK_SIZE = 1024;
     static final int A_INDEX_META_FACTOR = 32;
 
     static final int WRITE_STAGE_BUFFER_SIZE = STAGE_MSG_BYTE_LENGTH * 1024;

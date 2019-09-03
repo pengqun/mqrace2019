2c9804d6b00285fb4fe2de2f7d8f0a3fcdf50009
楚衡 <pengqun.pq@alibaba-inc.com>
2019-09-03 22:01:44 +0800

    run for score

--- src/main/java/io/openmessaging/Constants.java
+++ src/main/java/io/openmessaging/Constants.java
@@ -17,10 +17,11 @@ class Constants {
 
     static final int A_INDEX_MAIN_BLOCK_SIZE = 1024 * 9;
     static final int A_INDEX_SUB_BLOCK_SIZE = 1024 * 3;
-    static final int A_INDEX_META_FACTOR = 64;
+    static final int A_INDEX_META_FACTOR = 32;
 
     static final int WRITE_STAGE_BUFFER_SIZE = STAGE_MSG_BYTE_LENGTH * 1024 * 4;
     static final int WRITE_A_BUFFER_SIZE = KEY_A_BYTE_LENGTH * 1024 * 4;
+    static final int WRITE_AI_BUFFER_SIZE = KEY_A_BYTE_LENGTH * 1024 * 4;
     static final int WRITE_BODY_BUFFER_SIZE = BODY_BYTE_LENGTH * 1024 * 4;
 
     static final int READ_STAGE_BUFFER_SIZE = STAGE_MSG_BYTE_LENGTH * 1024 * 16;
@@ -29,7 +30,6 @@ class Constants {
     static final int READ_A1_BUFFER_SIZE = KEY_A_BYTE_LENGTH * 1024 * 16;
     static final int READ_A2_BUFFER_SIZE = KEY_A_BYTE_LENGTH * 1024 * 16;
 
-    static final int WRITE_AI_BUFFER_SIZE = KEY_A_BYTE_LENGTH * 1024;
 //    static final int READ_AIM_BUFFER_SIZE = KEY_A_BYTE_LENGTH * A_INDEX_META_FACTOR;
     static final int READ_AIM_BUFFER_SIZE = KEY_A_BYTE_LENGTH * 1024 * 16;
     static final int READ_AIS_BUFFER_SIZE = KEY_A_BYTE_LENGTH * 1024 * 16;
